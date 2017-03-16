92ec0d810f8cb9cd937e0d89b750b5860fc9b286
Federico Naveira <federiconaveira@gmail.com>
2017-03-16 13:05:28 -0300

    Added socket.io and express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const classes = require('./classes');
-const PedrinGaul = require('./players/PedrinGaul');
+var app = require('express')()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+const port = 3000
+const clients = []
+
+// temporal includes
+const classes = require('./classes')
+const PedrinGaul = require('./players/PedrinGaul')
 
 
 const arena = new classes.Arena();
@@ -10,6 +17,39 @@ for (var i = 0; i < 3; i++) {
   // app.arena.addPlayer(new MarioBaracus())
 }
 
+app.get('/', function(req, res) {
+  res.send('hi world')
+})
+app.get('/start', function(req, res) {
+  arena.start();
+  res.send('Arena has started')
+})
+app.get('/stop', function(req, res) {
+  arena.stop();
+  res.send('Arena has stoped')
+})
+app.get('/addPlayer', function(req, res) {
+  arena.addPlayer(new PedrinGaul())
+  res.send('Player has added')
+})
 
 
-arena.start();
+
+
+server.listen(port, function() {
+    console.log('Server running at http://localhost:' + port)
+});
+
+io.on('connection', function(client) {
+  clients.push(client)
+  console.log('Added new connection')
+});
+
+
+sendStatus()
+function sendStatus () {
+  for (var i = 0; i < clients.length; i++) {
+    clients[i].emit('update_finish', JSON.stringify(arena.elements));
+  }
+  setTimeout(sendStatus, 1000);
+}
